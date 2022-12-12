c6a42b538de8d5d344b9edb1087f28d09ddf0d33
khalid <105001050+Khallihub@users.noreply.github.com>
2022-12-13 00:43:04 +0300

    Update Domino.py

--- Domino.py
+++ Domino.py
@@ -1,9 +1,11 @@
-def Domino(a,b):
-    c = a/2
-    d = b/2
-    if a%2==0:
-        return c*b
-    elif b%2==0:
-        return d*a
-    else:
-        return a*(b-1)/2 + (a-1)/2
+x = input().split()
+a = int(x[0])
+b = int(x[1])
+c = a/2
+d = b/2
+if a%2==0:
+    print(int( c*b))
+elif b%2==0:
+    print( int(d*a))
+else:
+    print( int(a*(b-1)/2 + (a-1)/2))
