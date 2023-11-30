bff1ed8dd82f7755bcceef0e4a1c82fee0958d93
khalid <105001050+Khallihub@users.noreply.github.com>
2023-11-30 18:02:10 +0300

    Added solution for Goal Parser Interpretation

---
+++ week1/goal-parser-interpretation.py
@@ -0,0 +1,17 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        s = ""
+        i = 0
+        while i < len(command):
+            print(command[i])
+            if command[i] == "G":
+                s += "G"
+                i += 1
+            elif command[i] == "(" and command[i+1] == ")": 
+                s += "o"
+                i += 2
+            elif command[i] == "(" and command[i+1] == "a":
+                s += "al"
+                i += 4
+
+        return s
